0bfe95d521629c8521236f8a675efaf46486f44d
Ruizhou Ding <rzhding@gmail.com>
2020-01-11 00:20:47 -0500

    lightnn

--- models/fcn8s_lightnn.py
+++ models/fcn8s_lightnn.py
@@ -99,6 +99,7 @@ class FCN8s_lightnn(nn.Module):
                 m.weight.org = m.weight.data.clone()
                 # m.weight.data = Quantize_lightnn(m.weight.org, 'det', numShifts=2, shiftBits=7)
                 std = m.weight.data.std()
+                print('std: {}'.format(std))
                 m.weight.data = ((m.weight.data / std) * 256.).round() / 256. * std
 
         x_size = x.size()
